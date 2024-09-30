2b9441d55baf210a352a64a243ce73d85741b731
Olivier Wouters <olivier5741@gmail.com>
2024-09-30 17:08:26 +0200

    Wake up API in documentation

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const graphql = JSON.stringify({
+  query: "{\n  activities(limit: 1) {\n    name\n  }\n}",
+  variables: {},
+});
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: graphql,
+  redirect: "follow",
+};
+
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
+// Wake API up
+fetch("https://crisp-ferret-7818.ddn.hasura.app/graphql", requestOptions);
